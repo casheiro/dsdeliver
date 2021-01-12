7897b1f56e3ce69bc7ffc1df980382aa93bb47c1
Antonio Miranda <netomantonio@gmail.com>
2021-01-12 15:56:34 -0300

    Add Orders list

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 export default function Header() {
-  return (
-    <View style={styles.container}>
-        <Image source={require('../assets/logo.png') }/>
-      <Text style={styles.text}>DS Delivery</Text>
+  const navigation = useNavigation();
 
-    </View>
+  const handleOnPress = () => {
+    navigation.navigate('Home');
+  }
+  return (
+    <TouchableWithoutFeedback onPress={handleOnPress}>
+      <View style={styles.container}>
+        <Image source={require('../assets/logo.png')} />
+        <Text style={styles.text}>DS Delivery</Text>
+
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
